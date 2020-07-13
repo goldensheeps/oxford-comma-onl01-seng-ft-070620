f1d688c86291ef9a33713bda08dc40b3b16c435a
Timothy Lalli <trlalli92@gmail.com>
2020-07-13 18:54:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,39 +8,3 @@ def oxford_comma(array)
     array.join(', ')
   end
 end
-
-
-# Write a method oxford_comma that takes an argument array of string elements and converts it into a string using the Oxford comma. For example, the array ["fiddleheads","okra","kohlrabi"] should get converted to the string "fiddleheads, okra, and kohlrabi".
-# << array.push "and" -1;
-
-
-# def badge_maker(name)
-#   return "Hello, my name is #{name}."
-# end
-
-
-# def batch_badge_creator(speakers)
-#   badges = []
-#   speakers.each do |name|
-#     badges << badge_maker(name)
-#   end
-#   return badges 
-# end
-
-# def assign_rooms(speakers)
-#   new_array = []
-#   speakers.each_with_index do |name, index_room|
-#     new_array <<  "Hello, #{name}! You'll be assigned to room #{index_room + 1}!"
-#   end
-#   return new_array
-# end
-
-
-# def printer(speakers)
-#   batch_badge_creator(speakers).each do |badge|
-#     puts badge
-#   end
-#   assign_rooms(speakers).each do |room|
-#     puts room
-#   end
-# end
